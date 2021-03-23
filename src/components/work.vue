8eabf090abe22c02012df1dab4d814dce4ba1e87
kusiro <jackysoon871008@gmail.com>
2021-03-23 21:23:51 +0800

    Use v for in work page

--- src/components/work.vue
+++ src/components/work.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="work">
     <h1 class="title-text">Work Experience</h1>
-    <div class="work-experience">
-      <div class="image-head"><img src="../assets/img/NCHC.jpg" alt=""></div>
-      <div class="content">
-        <h1 class="title">國家高速網路運算中心</h1>
-        <h2 class="job-name">網頁開發工讀生</h2>
-        <ul>
-          <li>水利署計畫: 自動化淹水監控系統</li>
-          <li>串接全台 cctv 與政府提供之淹水數據 api</li>
-        </ul>
-        <div class="tag-wrapper">
-          <div class="tag">PHP</div>
-          <div class="tag">AWS lambda</div>
-          <div class="tag">python</div>
+    <div class="feature-block">
+      <div class="timeline"></div>
+
+      <div class="card-wrapper">
+        <div class="work-experience" v-for="work in list" v-bind:key="work.id">
+          <div class="image-head"><img src={{work.logo}} alt=""></div>
+          <div class="content">
+            <h1 class="title">{{ work.title }}</h1>
+            <h2 class="job-name">{{ work.jobName }}</h2>
+            <ul>
+              <li v-for="w in work.list" v-bind:key="w.id">{{ w }}</li>
+            </ul>
+            <div class="tag-wrapper">
+              <div class="tag" v-for="tag in work.tags" v-bind:key="tag.id">{{ tag }}</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -22,8 +25,27 @@
 
 <script>
 export default {
-  name: 'work'
+  name: 'work',
+  data() {
+    return {
+      list: [
+        {
+          title: "國家高速網路運算中心",
+          logo: "../assets/img/NCHC.jpg",
+          jobName: "網頁開發工程師",
+          list: ["水利署計畫", "串接全台 cctv 與政府提供之淹水數據"],
+          tags: ["PHP", "AWS lambda", "python"]
+        }, {
+          title: "Owner",
+          jobName: "Jobname",
+          list: ["What", "you", "do"],
+          tags: ["tag", "tag", "tag"]
+        }
+      ]
+    }
+  }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -31,65 +53,82 @@ export default {
 #work {
     min-height: 100vh;
     padding: 0 6em;
-    display: flex;
-    flex-direction: column;
 
     .title-text {
       color: $font-color-black;
       font-size: 64px;
-      margin: 0;
+      height: 1em;
+      flex: 100%;
       display: inline-block;
       margin: 1.6em 0;
       font-family: $font-family;
       display: block;
     }
-    .work-experience {
-      // border-right: solid 1em $main-color-black;
-      display: flex;
-      border-radius: 3px;
-      align-items: center;
-      padding: 1.6em;
-      height: 16em;
-      flex-wrap: nowrap;
-      position: relative;
-      background-color: #e2decd;
-      font-family: $font-family;
 
-      .image-head {
-        height: 100%;
-        margin-right: 2em;
-        img{ height: 100%;}
+    .feature-block {
+      display: flex;;
+
+      .timeline {
+        flex: 1;
+        border: solid 1px;
+        margin-right: 1em;
       }
-      .content {
-        h1 {
-          font-size: 24px;
-          margin: 0;
-        }
-        h2 {
-          font-size: 20px;
-          margin: 0;
-          font-weight: normal;
-        }
-        ul {
-          margin: 1em 0;
-          padding-left: 1em;
-          list-style: circle;
-          li {
-            margin: .5em 0;
-          }
-        }
-        .tag-wrapper {
+      .card-wrapper {
+        display: flex;
+        flex: 12;
+        flex-direction: column;
+          
+        .work-experience {
+          // border-right: solid 1em $main-color-black;
           display: flex;
-          margin-top: 1.5em;
-          .tag {
-            margin-right: .5em;
-            border: solid 1px $font-color-black;
-            color: $font-color-black;
-            border-radius: 7px;
-            padding: .3em;
+          border-radius: 10px;
+          align-items: center;
+          padding: 0 2em;
+          width: 70%;
+          flex-wrap: nowrap;
+          position: relative;
+          background-color: #e2decd;
+          font-family: $font-family;
+
+          .image-head {
+            height: 75%;
+            margin-right: 2em;
+            img{ height: 100%;}
+          }
+          .content {
+            h1 {
+              font-size: 24px;
+              margin: 0;
+            }
+            h2 {
+              font-size: 20px;
+              margin: 0;
+              font-weight: normal;
+            }
+            ul {
+              margin: 1em 0;
+              padding-left: 1em;
+              list-style: circle;
+              li {
+                margin: .5em 0;
+              }
+            }
+            .tag-wrapper {
+              display: flex;
+              margin-top: 1.5em;
+              .tag {
+                margin-right: .5em;
+                border: solid 1px $font-color-black;
+                color: $font-color-black;
+                border-radius: 7px;
+                padding: .3em;
+              }
+            }
           }
         }
       }
     }
+
+
 }
 </style>
